c09b17f54b0b8e29650efa35914a0a663fc64f74
hecey <hecey@hotmail.com>
2020-11-23 06:10:23 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const cors = require("cors");
+const express = require("express");
+const bodeparser = require("body-parser");
+const { connect } = require("mongoose");
+const { success, error } = require("consola");
+
+//Bring contants in th App
+const { DB, PORT } = require("./config");
+//Initialize the application
+const app = express();
+//Middlewares
+app.use(cors());
+app.use(bodeparser.json());
+
+//User router middleware
+app.use('/api/users', require("./routes/users"));
+
+const startapp = async () => {
+    try {
+        //connecting with database
+        await connect(DB, {
+            useFindAndModify: true,
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+        success({
+            message: `Successfully conected with the Database \n${DB} `,
+            badge: true
+        });
+        // start listening for server on PORT
+        app.listen(PORT, () => success({
+            message: `Server started on port: ${PORT}`,
+            badge: true
+        }));
+    } catch (error) {
+        error({
+            message: `Error conecting with the Database \n${DB}`,
+            badge: true
+        });
+        //startapp();
+    }
+};
+startapp();
+
---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+
+//User Registration
+router.post('/resgister-user', async (req, res)=>{
+
+});
+//Admin Registration Mode
+router.post('/resgister-admin', async (req, res)=>{
+    
+});
+//Super Admin Registration Mode
+router.post('/resgister-super-admin', async (req, res)=>{
+    
+});
+//User Login
+router.post('/login-user', async (req, res)=>{
+
+});
+//Admin Login Mode
+router.post('/login-admin', async (req, res)=>{
+    
+});
+//Super Admin Login Mode
+router.post('/login-super-admin', async (req, res)=>{
+    
+});
+//User Protected
+router.post('/user-profile', async (req, res)=>{
+
+});
+//Admin Protected Mode
+router.post('/admin-profile', async (req, res)=>{
+    
+});
+//Super Admin Protected Mode
+router.post('/super-admin-profile', async (req, res)=>{
+    
+});
+module.exports = router;
